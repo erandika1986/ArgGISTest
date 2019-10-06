3065580310c9b838713862e28c339b80e4f27efe
iTexphere <chathura.jeev.ranasingha@gmail.com>
2019-10-06 07:21:10 +0530

    sample build project added

---
+++ ArgGISTest/wwwroot/precache-manifest.e040ebf1f4d9d78a34d69c9ed5b06e4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "980a06d2d9b4d3243155736660e44fb5",
+    "url": "/testing/build/index.html"
+  },
+  {
+    "revision": "2eb080d29278d64b81ff",
+    "url": "/testing/build/static/css/2.7d36abf2.chunk.css"
+  },
+  {
+    "revision": "492c4d9af8ef5a9f0cfb",
+    "url": "/testing/build/static/css/main.ee704fd1.chunk.css"
+  },
+  {
+    "revision": "2eb080d29278d64b81ff",
+    "url": "/testing/build/static/js/2.6c526375.chunk.js"
+  },
+  {
+    "revision": "492c4d9af8ef5a9f0cfb",
+    "url": "/testing/build/static/js/main.42f40395.chunk.js"
+  },
+  {
+    "revision": "ad059f4440bd428c7be2",
+    "url": "/testing/build/static/js/runtime-main.e3790301.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/testing/build/static/media/logo.25bf045c.svg"
+  }
+]);
