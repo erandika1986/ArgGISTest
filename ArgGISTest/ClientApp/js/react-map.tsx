98e3b5c63c5409cc13bec290e2945a8addcc230c
erandika86 <30391205+erandika86@users.noreply.github.com>
2019-10-08 16:36:11 +0530

    React App added to the VS Razor page project

---
+++ ArgGISTest/ClientApp/js/react-map.tsx
@@ -0,0 +1,9 @@
+﻿import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import App from './react-map/App';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('react-map-app')
+);
---
+++ ArgGISTest/wwwroot/dist/react-map.js
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './react-map/App';
+ReactDOM.render(React.createElement(App, null), document.getElementById('react-map-app'));
+//# sourceMappingURL=react-map.js.map
