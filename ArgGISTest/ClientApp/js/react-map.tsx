1b195dfe7c8d6d6a197129595094e4a80181d5dd
erandika86 <30391205+erandika86@users.noreply.github.com>
2019-10-09 10:53:11 +0530

    file deleted

--- ArgGISTest/ClientApp/js/react-map.tsx
+++
@@ -1,9 +0,0 @@
-﻿import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-import App from './react-map/App';
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('react-map-app')
-);
--- ArgGISTest/wwwroot/dist/react-map.js
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './react-map/App';
-ReactDOM.render(React.createElement(App, null), document.getElementById('react-map-app'));
-//# sourceMappingURL=react-map.js.map
